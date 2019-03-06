8d859f3e1810aeaa6308231d8baf742b1804dc6e
VikaYallina <vicka.vicka.vicka111@gmail.com>
2019-03-06 20:29:18 +0300

    first commit

---
+++ Task 10.pas
@@ -0,0 +1,64 @@
+﻿type list = record
+     next:^list;
+     info:integer;
+     end;
+     point = ^list;
+var head,head2,tail,p,q:point; 
+i:integer; 
+
+procedure Addelem (var t,f,l:point); //процедура добавления элемента в список, нахождение головы и хвоста списка
+  var y:point;
+i:integer;
+begin
+ new (y);
+ readln(i);
+ y^.info:=i;
+ if f=nil then //если адрес головы пуст, то адрес головы и хвоста равен только что  добавленному
+  begin
+   f:=y;
+   t:=y;
+   l:=y;
+   end
+   else if f^.next=nil then //если голова не указывает на следующий элемент, то задается связь головы введенным элементом, хвостом становится только что введенный элемент
+   begin
+   f^.next:=y;
+   l:=y;
+   t:=y;
+   end
+     else begin
+     l^.next:=y;
+     t:=y;
+     l:=y;
+     end;
+ end;
+ 
+ function FindP(he,x:point):point; // функция нахождения адреса элемента, предыдущего данному
+ var l:point;
+   begin
+     l:=he;
+     while l^.next<>x do
+     l:=l^.next;
+     result:=l;
+   end;
+   
+begin 
+   for i:=1 to  6 do 
+   Addelem(q,head,tail); //ввод списка
+ head2:=tail; //присваиваем новому адресу головы адрес хвоста
+ q:=tail;
+ p:=nil;
+  while p<>head do
+   begin
+   p:=FindP(head,q);//находим адрес элемента, предшевствующего данному
+   q^.next:=p; //связываем данную ячейку с предшевствующей ей
+   q:=p;
+   if p=head then
+      p^.next:=nil; //когда адрес элемента станет равен адресу первоначальной головы списка, опустошаем указатель на следующий элемент
+  end; 
+writeln('Вывод списка');
+while head2<>nil do
+  begin
+  writeln(head2^.info);
+  head2:=head2^.next;
+  end;
+end. 
